47fb487503a96694856332c43e476fa0260a8867
Heriniaina1905 <heriniaina.randrianasolo@hotmail.com>
2020-11-24 09:07:07 +0300

    NOV 24 2020, FIRST COMMIT

--- src/main/java/tv/nosy/cli/controller/CRUDController.java
+++ src/main/java/tv/nosy/cli/controller/CRUDController.java
@@ -9,15 +9,18 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import tv.nosy.cli.models.dao.Admin;
 import tv.nosy.cli.models.dao.custom.Achat;
 import tv.nosy.cli.models.dao.custom.Box;
+import tv.nosy.cli.models.services.AdminImplDAO;
 import tv.nosy.cli.models.services.custom.BoxImp;
 
 @Controller
 @RequestMapping(value = {"/list", "/crud"})
 public class CRUDController {
 
-    protected BoxImp bi;
+    protected BoxImp bi = new BoxImp();
+    protected AdminImplDAO aid = new AdminImplDAO();
 
     @RequestMapping(value = {"box"}, method = RequestMethod.GET)
     public String getListBox(Model model, HttpSession session){
@@ -32,6 +35,20 @@ public class CRUDController {
         }
 
         return "list/box";
+    }
+    @RequestMapping(value = {"admin"}, method = RequestMethod.GET)
+    public String getListAdmin(Model model, HttpSession session){
+        if(session.getAttribute("admin")==null){
+            return "redirect:/";
+        }
+        try{
+            List<Admin> adm = aid.getAllAdmin();
+            model.addAttribute("admin", adm);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
+        return "list/admin";
     }
 
     @RequestMapping(value = {"insertBox"}, method = RequestMethod.POST)
